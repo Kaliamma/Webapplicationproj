41a99255a529334fcd000df27b73c5aa487dd1fc
Kaliamma <45587475+Kaliamma@users.noreply.github.com>
2019-03-12 18:07:39 +0530

    Add files via upload

---
+++ Frontendform.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Frontendform.aspx.vb" Inherits="WebApplication3.Frontendform" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Google Assignment</title>
+    <style type="text/css">
+        #Image1{
+            background-image:url('');
+
+        }
+        #Button2 {
+            width: 281px;
+            height: 39px;
+            margin-left: 302px;
+            margin-top: 16px;
+        }
+        .newStyle1 {
+            font-style: normal;
+        }
+        #Button3 {
+            width: 279px;
+            margin-left: 40px;
+            height: 39px;
+        }
+        #Button1 {
+            margin-left: 297px; 
+            margin-bottom: 47px;
+            width:285px;
+            height:77px;
+        }
+        body {
+            background-color:lightcyan;
+            height: 433px; 
+            width: 906px;
+        }
+        #FileUpload1{
+            margin-left: 40px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" src="App_Data/gbanner.png" Height="87px" Width="267px"/>
+            <asp:Image ID="Image2" runat="server" src="App_Data/signin.png" Height="85px" Width="351px"/>
+        </div>
+        <div>
+        </div>
+        <p style="height: 55px">
+            <asp:Button ID="Button1" runat="server"  Text="Sign-in with GMAIL"  tabindex="1"/>  
+        </p>
+        <br /><br />
+        <asp:Label ID="Label1" runat="server" style="font-size: medium" Text="File Upload:"></asp:Label>
+        <asp:FileUpload ID="FileUpload1" runat="server" tabindex="2" Width="465px" /><br />
+        
+        <asp:Button id="Button2" runat="server" text="Upload Files GDrive" tabindex="3"/>
+        <p>
+        <asp:Label ID="Label2" runat="server" Text="Are you ready?Click and download the file."></asp:Label>
+        <asp:Button id="Button3" runat="server" text="Download Files GDrive" tabindex="4"/>
+
+        </p>
+    
+    </form>
+    
+
+
+</body>
+</html>
---
+++ Frontendform.aspx.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Threading
+Imports System.Threading.Tasks
+
+Imports Google
+Imports Google.Apis.Auth.OAuth2
+Imports Google.Apis.Drive.v2
+Imports Google.Apis.Drive.v2.Data
+Imports Google.Apis.Services
+
+Imports Google.Apis.Auth
+Imports Google.Apis.Download
+
+
+
+
+
+
+Public Class Frontendform
+
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Redirect("GmailPage.html")
+    End Sub
+
+
+    Protected Sub Button2_Click1(sender As Object, e As EventArgs) Handles Button2.Click
+        CreateService()
+        UploadFile()
+    End Sub
+    Private Sub UploadFile()
+        Throw New NotImplementedException()
+    End Sub
+    Private Service As DriveService = New DriveService
+    Private Cursors As Object
+    Public Property Cursor As Object
+
+    Private Sub CreateService()
+
+        Dim ClientId = "298236013091-6pcu43ooas74vrnpajku6fu6p4gct4qd.apps.googleusercontent.com "
+        Dim ClientSecret = "mFxduvUwzqqaENce1hOP_fOI"
+        Dim MyUserCredential As UserCredential = GoogleWebAuthorizationBroker.AuthorizeAsync(New ClientSecrets() With {.ClientId = ClientId, .ClientSecret = ClientSecret}, {DriveService.Scope.Drive}, "user", CancellationToken.None).Result
+            Service = New DriveService(New BaseClientService.Initializer() With {.HttpClientInitializer = MyUserCredential, .ApplicationName = "Google Drive VB Dot Net"})
+
+    End Sub
+
+    Private Sub UploadFile(FilePath As String)
+        Me.Cursor = Cursors.WaitCursor
+        If Service.ApplicationName <> "Google Drive VB Dot Net" Then CreateService()
+
+        Dim TheFile As New File()
+        TheFile.Title = "My document"
+        TheFile.Description = "A test document"
+        TheFile.MimeType = "text/plain"
+
+        Dim ByteArray As Byte() = System.IO.File.ReadAllBytes(FilePath)
+        Dim Stream As New System.IO.MemoryStream(ByteArray)
+
+        Dim UploadRequest As FilesResource.InsertMediaUpload = Service.Files.Insert(TheFile, Stream, TheFile.MimeType)
+
+        Me.Cursor = Cursors.Default
+        MsgBox("Upload Finished")
+    End Sub
+
+
+End Class
